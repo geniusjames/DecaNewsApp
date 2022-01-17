eb33a5cab891e8e7e30d601ccb78c1f6b5364e5d
James Anyanwu <jameslecturer@yahoo.com>
2022-01-17 22:32:09 +0100

    initial Commit

---
+++ DecaNews/DecaNews/View/OnboardingCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewCell.swift
+//  DecaNews
+//
+//  Created by Geniusjames on 17/01/2022.
+//
+
+import UIKit
+
+class OnboardingCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ DecaNews/DecaNews/View/OnboardingCollectionViewLayout.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingCollectionViewLayout.swift
+//  DecaNews
+//
+//  Created by Geniusjames on 17/01/2022.
+//
+
+import UIKit
+
+class OnboardingCollectionViewLayout: UICollectionViewLayout {
+
+}
