9d16c150bc800fd8c1fd5e756418ff1791196a30
James Anyanwu <jameslecturer@yahoo.com>
2022-02-01 10:20:02 +0100

    Update ViewModel.swift

--- DecaNews/DecaNews/View-Model/ViewModel.swift
+++ DecaNews/DecaNews/View-Model/ViewModel.swift
@@ -8,9 +8,9 @@
 import Foundation
 
 final class ViewModel {
-//    let firebaseService: FirebaseService
+  let firebaseService: FirebaseService
 
     init() {
-//      firebaseService = FirebaseService()
+     firebaseService = FirebaseService()
     }
 }
