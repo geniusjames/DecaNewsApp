1f006d0ed8adff3cf1777b091c341c26ff0dd18c
Thomas <akinniyithomas@yahoo.com>
2022-02-02 16:21:50 +0100

    topics ui done and background logic done

--- DecaNews/DecaNews/Coordinator/MainCoordinator.swift
+++ DecaNews/DecaNews/Coordinator/MainCoordinator.swift
@@ -10,6 +10,7 @@ import UIKit
 class MainCoordinator: Coordinator {
     private var window: UIWindow
     var controller: UINavigationController
+    let viewModel = ViewModel()
     init(window: UIWindow) {
         self.window = window
         controller = UINavigationController()
@@ -38,4 +39,29 @@ class MainCoordinator: Coordinator {
         window.rootViewController = controller
         window.makeKeyAndVisible()
     }
+
+    func openSignUp() {
+            let emailSignUpViewController = UIStoryboard(name: "EmailLogin", bundle: nil)
+                .instantiateViewController(withIdentifier: "EmailLoginViewController")
+            as? EmailLoginViewController
+            emailSignUpViewController?.viewModel = viewModel
+            guard let emailSignUpViewController = emailSignUpViewController else { return }
+            emailSignUpViewController.coordinator = self
+            controller.pushViewController(emailSignUpViewController, animated: true)
+            window.rootViewController = controller
+            window.makeKeyAndVisible()
+        }
+
+        func openTopics() {
+            let viewController = UIStoryboard(name: "EmailLogin", bundle: nil)
+                .instantiateViewController(withIdentifier: "SelectedTopicsViewController")
+            as? SelectedTopicsViewController
+            guard let viewController = viewController else { return }
+            viewController.coordinator = self
+            viewController.viewModel = viewModel
+            controller = UINavigationController()
+            controller.pushViewController(viewController, animated: true)
+            window.rootViewController = controller
+            window.makeKeyAndVisible()
+        }
 }
--- DecaNews/DecaNews/View/SelectedTopicsViewController.swift
+++ DecaNews/DecaNews/View/SelectedTopicsViewController.swift
@@ -9,12 +9,49 @@ import UIKit
 
 final class SelectedTopicsViewController: UIViewController {
 
+    @IBOutlet weak var gymFitnessButton: UIButton!
+    @IBOutlet weak var recipesButton: UIButton!
+    @IBOutlet weak var healthButton: UIButton!
+    @IBOutlet weak var sportsButton: UIButton!
+    @IBOutlet weak var travelButton: UIButton!
+    @IBOutlet weak var natureButton: UIButton!
+    @IBOutlet weak var entertainmentButton: UIButton!
+    @IBOutlet weak var educationButton: UIButton!
+    @IBOutlet weak var writingButton: UIButton!
+    @IBOutlet weak var architectureButton: UIButton!
+    @IBOutlet weak var photographyButton: UIButton!
+    @IBOutlet weak var bookButton: UIButton!
+    @IBOutlet weak var designButton: UIButton!
+    @IBOutlet weak var businessButton: UIButton!
+    @IBOutlet weak var musicButton: UIButton!
+    var buttonStore = [Int: Bool]()
+    
     var coordinator: MainCoordinator?
     var viewModel: ViewModel?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        var buttons: [UIButton] = [musicButton, businessButton, designButton, bookButton, photographyButton,
+                       architectureButton, writingButton, educationButton, entertainmentButton,
+                       natureButton, travelButton, sportsButton, healthButton, recipesButton, gymFitnessButton]
+        for button in buttons {
+            buttonStore[button.tag] = false
+            button.addTarget(self, action: #selector(buttonClicked), for: .touchUpInside)
+        }
         // Do any additional setup after loading the view.
+    }
+    
+    @objc
+    func buttonClicked(_ sender: Any) {
+        guard let button = sender as? UIButton, let buttonState = buttonStore[button.tag]  else { return }
+        if buttonState {
+            buttonStore[button.tag] = false
+            button.tintColor = UIColor(named: "grey")
+            button.setTitleColor(UIColor(named: "lightBlack"), for: .normal)
+        } else {
+            buttonStore[button.tag] = true
+            button.tintColor = UIColor(named: "lightBlack")
+            button.setTitleColor(UIColor(named: "offWhite"), for: .normal)
+        }
     }
 
     /*
@@ -27,4 +64,6 @@ final class SelectedTopicsViewController: UIViewController {
     }
     */
 
+    @IBAction func getStarted(_ sender: Any) {
+    }
 }
