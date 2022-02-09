fc9ba8821dd3c12a42f4ed7bf62977db382ef714
Usman Kulaha <32143087+UK818@users.noreply.github.com>
2022-02-09 05:52:00 +0100

    completed UI design and linked storyboard to Dashboard View Controller

---
+++ DecaNews/DecaNews/App/Dashboard/Controller/DashboardViewController.swift
@@ -0,0 +1,19 @@
+//
+//  DashboardViewController.swift
+//  DecaNews
+//
+//  Created by mac on 06/02/2022.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+	
+	var coordinator: MainCoordinator?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+}
