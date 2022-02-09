d228c5b7211c04917feaadfbe2c1ebce109d0346
SharonB9 <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>
2022-02-10 00:43:03 +0100

    like button functional

--- DecaNews/DecaNews/View/DetailsViewController/NewsDetailsViewController.swift
+++ DecaNews/DecaNews/View/DetailsViewController/NewsDetailsViewController.swift
@@ -3,6 +3,14 @@ import UIKit
 
 class DetailsViewController: UIViewController {
     var coordinator: MainCoordinator?
+    var countClickedButtons: Int = 0
+//    {
+//         didSet {
+//              let text = String(countClickedButtons)
+//             likeButtons.setTitle(text, for: .normal)
+//         }
+//    }
+
     @IBOutlet weak var backNavigationButton: UIButton!
     @IBOutlet weak var newsImage: UIImageView!
     
@@ -21,10 +29,27 @@ class DetailsViewController: UIViewController {
     
     @IBOutlet weak var usersName: UILabel!
     @IBOutlet weak var imageOfUser: UIImageView!
-    @IBAction func likeButton(_ sender: Any) {
-    }
+    
+    @IBOutlet weak var likeButtons: UIButton!
+    @IBAction func likeButton(_ sender: DetailsViewController) {
+//        self.likes = !self.likeButtons.isSelected
+//        self.likeButtons.isSelected = self.likes
+        if likeButtons.tag == 0 {
+            likeButtons.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .normal)
+            likeButtons.tag = 1
+            let text = String(countClickedButtons + 1)
+            likeButtons.setTitle(text, for: .normal)
+        } else {
+            likeButtons.setImage(UIImage(systemName: "hand.thumbsup"), for: .normal)
+            likeButtons.tag = 0
+            let text = String(countClickedButtons)
+            _ = String(countClickedButtons - 1)
+            likeButtons.setTitle(text, for: .normal)
+        }
+        }
     
     @IBAction func commentButton(_ sender: Any) {
+        
     }
     
     @IBOutlet weak var newsText: UITextView!
@@ -33,7 +58,16 @@ class DetailsViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .white
         newsImage.backgroundColor = .red
+        newsText.isEditable = false
+//        let range = NSMakeRange(newsText.text.characters.count - 1, 0)
+//        newsText.scrollRangeToVisible(range)
+//        self.likeButtons.isSelected = self.likes
+
+//              // set the titles for the likes button per state
+//              self.likesButton.setTitle("Like", forState: .normal)
+//              self.likesButton.setTitle("Likes", forState: .selected)
     }
+    
     
     
     
