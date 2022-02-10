528fbb58815787a3bcd7ac5a4b4f5de765d45ad0
SharonB9 <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>
2022-02-10 01:21:45 +0100

    detailsViewController re-organized

--- DecaNews/DecaNews/View/DetailsViewController/NewsDetailsViewController.swift
+++ DecaNews/DecaNews/View/DetailsViewController/NewsDetailsViewController.swift
@@ -2,38 +2,26 @@ import Foundation
 import UIKit
 
 class DetailsViewController: UIViewController {
+    
     var coordinator: MainCoordinator?
-    var countClickedButtons: Int = 0
-//    {
-//         didSet {
-//              let text = String(countClickedButtons)
-//             likeButtons.setTitle(text, for: .normal)
-//         }
-//    }
-
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureView()
+    }
     @IBOutlet weak var backNavigationButton: UIButton!
     @IBOutlet weak var newsImage: UIImageView!
-    
     @IBAction func bookMarkButton(_ sender: Any) {
     }
-    
     @IBAction func textEditorButton(_ sender: Any) {
     }
     @IBOutlet weak var newsCategory: UIButton!
-    
     @IBOutlet weak var newsMinuteRead: UILabel!
-    
     @IBOutlet weak var timeFromNewsPost: UILabel!
-    
     @IBOutlet weak var newsTitle: UILabel!
-    
     @IBOutlet weak var usersName: UILabel!
     @IBOutlet weak var imageOfUser: UIImageView!
-    
     @IBOutlet weak var likeButtons: UIButton!
-    @IBAction func likeButton(_ sender: DetailsViewController) {
-//        self.likes = !self.likeButtons.isSelected
-//        self.likeButtons.isSelected = self.likes
+    @IBAction func likeButton(_ sender: Any) {
         if likeButtons.tag == 0 {
             likeButtons.setImage(UIImage(systemName: "hand.thumbsup.fill"), for: .normal)
             likeButtons.tag = 1
@@ -46,29 +34,16 @@ class DetailsViewController: UIViewController {
             _ = String(countClickedButtons - 1)
             likeButtons.setTitle(text, for: .normal)
         }
-        }
-    
+    }
     @IBAction func commentButton(_ sender: Any) {
         
     }
-    
     @IBOutlet weak var newsText: UITextView!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var countClickedButtons: Int = 0
+    func configureView() {
         view.backgroundColor = .white
-        newsImage.backgroundColor = .red
+        newsImage.backgroundColor = .gray
         newsText.isEditable = false
-//        let range = NSMakeRange(newsText.text.characters.count - 1, 0)
-//        newsText.scrollRangeToVisible(range)
-//        self.likeButtons.isSelected = self.likes
-
-//              // set the titles for the likes button per state
-//              self.likesButton.setTitle("Like", forState: .normal)
-//              self.likesButton.setTitle("Likes", forState: .selected)
     }
-    
-    
-    
-    
 }
