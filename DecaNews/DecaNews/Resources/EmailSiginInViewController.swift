6ba5a86ff6897f9e88e48a3f6719ac6fae924a9b
James Anyanwu <jameslecturer@yahoo.com>
2022-02-17 00:49:50 +0100

    removed previous toggle function

--- DecaNews/DecaNews/Resources/EmailSiginInViewController.swift
+++ DecaNews/DecaNews/Resources/EmailSiginInViewController.swift
@@ -8,7 +8,6 @@ import UIKit
 
 final class EmailSiginInViewController: UIViewController {
     var showPassword: Bool = false
-    let button = UIButton()
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var loginButton: UIButton!
@@ -20,47 +19,23 @@ final class EmailSiginInViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        configurePasswordField()
         loginButton.addTarget(self, action: #selector(login), for: .allTouchEvents)
         emailTextField.addTarget(self, action: #selector(validateInputs), for: .allEditingEvents)
         passwordTextField.addTarget(self, action: #selector(validateInputs), for: .allEditingEvents)
     }
 
-    func configurePasswordField() {
-        let eyeView = UIView()
-        eyeView.translatesAutoresizingMaskIntoConstraints = false
-        eyeView.widthAnchor.constraint(equalToConstant: 40).isActive = true
-        eyeView.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        button.frame = CGRect(x: 0, y: 0, width: 15, height: 15)
-        passwordTextField.rightView = eyeView
-        eyeView.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.centerXAnchor.constraint(equalTo: eyeView.centerXAnchor).isActive = true
-        button.centerYAnchor.constraint(equalTo: eyeView.centerYAnchor).isActive = true
-        button.setImage(UIImage(imageLiteralResourceName: "eye-slash"), for: .normal)
-        button.addTarget(self, action: #selector(showOrHidePassword), for: .touchUpInside)
-        passwordTextField.rightViewMode = .always
-    }
-
-    @objc func showOrHidePassword() {
-        showPassword ? { button.setImage(UIImage(imageLiteralResourceName: "eye"), for: .normal)
-            passwordTextField.isSecureTextEntry = false
-            showPassword = false
-        }() : {button.setImage(UIImage(imageLiteralResourceName: "eye-slash"), for: .normal)
-            passwordTextField.isSecureTextEntry = true
-            showPassword = true
-        }()
-    }
     @objc func login() {
         guard let emailAddress = emailTextField.text
         else {return}
         guard let password = passwordTextField.text
         else {return}
         if emailAddress.isValidEmail && password.isValidPassword {
+            print("seen")
             servicesViewModel?.firebaseService.signIn(emailAddress, password) {result in
+                print("reached")
+                
                 switch result {
                 case .success(_:):
-                    print("go to home...")
                     self.navigateHome?()
                 case .failure(let error):
                     UIView.animate(withDuration: 3) {
@@ -72,6 +47,9 @@ final class EmailSiginInViewController: UIViewController {
                     }
                }
             }
+        }
+        else {
+       print("here")
         }
     }
 
