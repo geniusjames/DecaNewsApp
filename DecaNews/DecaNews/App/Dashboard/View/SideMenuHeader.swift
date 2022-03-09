fbd86979ef2944e617845c55f766a74566e4e322
Ebele <enedosa@yahoo.com>
2022-03-09 19:08:46 +0100

    modified sideMenuHeader

--- DecaNews/DecaNews/App/Dashboard/View/SideMenuHeader.swift
+++ DecaNews/DecaNews/App/Dashboard/View/SideMenuHeader.swift
@@ -28,7 +28,7 @@ class SideMenuHeader: UITableViewHeaderFooterView {
         imageView.layer.cornerRadius = imageView.frame.size.width / 2
         let nameLabel = UILabel()
         nameLabel.textColor = .white
-        nameLabel.text = user?.displayName ?? "Some Text"
+        nameLabel.text = user?.displayName ?? "Name"
         addSubviews(imageView, nameLabel)
         imageView.centerInSuperview()
         nameLabel.anchored(top: imageView.bottomAnchor, leading: nil, bottom: nil, trailing: nil, padding: UIEdgeInsets(top: 5, left: 0, bottom: 0, right: 0))
