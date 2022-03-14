60ee26a5319f493dc544c73ad7eae1106535307b
tomisin <tomisin18@gmail.com>
2022-03-14 16:34:32 +0100

    Fix build error

--- DecaNews/DecaNews/App/Dashboard/Controller/SideMenuViewController.swift
+++ DecaNews/DecaNews/App/Dashboard/Controller/SideMenuViewController.swift
@@ -13,34 +13,34 @@ class SideMenuViewController: UITableViewController {
     var viewModel: MenuTableViewModel?
     
     var didSelectMenuOption: ((MenuOption) -> Void)?
-
+    
     init() {
         super.init(nibName: nil, bundle: nil)
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.backgroundColor = .black
         tableView.register(SideMenuHeader.self, forHeaderFooterViewReuseIdentifier: "SideMenuHeader")
-//        tableView.tableFooterView = label
+        //        tableView.tableFooterView = label
     }
-
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return menuItem.count
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if indexPath.row == 0 {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: SideMenuTableViewCell.identifier, for: indexPath) as? SideMenuTableViewCell else {
                 return UITableViewCell()
             }
             let user = viewModel?.getUserDetails()
-			cell.profileName.text = user?.displayName
+            cell.profileName.text = user?.displayName
             cell.profileImage.layer.cornerRadius = cell.profileImage.frame.size.width / 2
             return cell
         } else {
@@ -53,24 +53,25 @@ class SideMenuViewController: UITableViewController {
                 cell.imageView?.image = UIImage(named: imageName)
             }
             return cell
+        }
     }
-
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         dismiss(animated: false, completion: {
             self.didSelectMenuOption?(self.menuItem[indexPath.row - 1])
-        }) 
+        })
     }
-
+    
     func isIndexGreaterThanSix(in list: [String]) -> Int {
         return list.count
     }
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        if indexPath.row == 0 {
-//            return 180
-//        } else {
-            return 60
-//        }
+        //        if indexPath.row == 0 {
+        //            return 180
+        //        } else {
+        return 60
+        //        }
     }
     
     override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
